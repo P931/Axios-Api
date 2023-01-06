795e4c292e840ba44a3131c4d80dab9a3f31b0dc
Princehingrajiya <hingrajiyaprinceliladharbhai@gmail.com>
2023-01-06 22:49:53 +0530

    axios-api-06-01-2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [Data, setData] = useState();
+  const [isError, setisError] = useState("");
+  const [Load, setLoad] = useState();
+
+  const getapidata = async () => {
+    try {
+      const res = await axios.get("https://dummyjson.com/users");
+      console.log(res.data);
+      setData(res.data);
+    } catch (error) {
+      console.log(`your data is not get ${error}`);
+      setisError(error.message);
+
+      // if (error) {
+      //   console.log(`your data is not get ${error}`);
+      //   setisError(error.message);
+      // } else {
+      //   console.log("loading your data");
+      //   setLoad(Load);
+      // }
+    } finally {
+      console.log("loading your data");
+      <div className="load">Loading....</div>;
+      setLoad(Load);
+    }
+  };
+
+  useEffect(() => {
+    // axios
+    //   .get("https://dummyjson.com/users")
+    //   .then((res) => setData(res.data))
+    //   .then((res) => console.log(res.data))
+    //   .catch((error) => setisError(error.message));
+    getapidata();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="head">Dummy Data Getting Using The Axios Api</h1>
+
+      <div className="details">
+        {Data ? (
+          Data.users &&
+          Data.users.map((post) => {
+            const { id, firstName, lastName, maidenName, age } = post;
+            return (
+              <>
+                <div className="card">
+                  <p>Id: {id}</p>
+                  <p>FirstName: {firstName}</p>
+                  <p>LastName: {lastName}</p>
+                  <p>MaidenName: {maidenName}</p>
+                  <p>Age: {age}</p>
+                </div>
+                {/* <div className="load">Loading....</div> */}
+              </>
+            );
+          })
+        ) : (
+          <>
+            {isError !== "" && (
+              <h2 className="err">{`User data is not get ${isError}`}</h2>
+            )}
+          </>
+        )}
+        {Load ? <div className="load">Loading....</div> : null}
+      </div>
+    </>
   );
 }
 
